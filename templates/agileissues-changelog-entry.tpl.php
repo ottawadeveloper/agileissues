f49e99db0255bbd1181cf661435b54ce015fd5a8
Robert Turnbull <rturnbul@uottawa.ca>
2014-11-21 15:01:16 -0500

    fixed changelog template

--- templates/agileissues-changelog-entry.tpl.php
+++ templates/agileissues-changelog-entry.tpl.php
@@ -1,7 +1,7 @@
 <?php
 
 echo '<div class="agile-changenote">';
-$user = user_load($change['#uid']);
+$user = user_load($change['#user']);
 echo '<span class="agile-changenote-person">' . $user->name . '</span> changed ';
 echo '<span class="agile-changenote-label">' . $change['#label'] . '</span> from ';
 echo '<span class="agile-changenote-old">[' . $change['#old'] . ']</span> to';
