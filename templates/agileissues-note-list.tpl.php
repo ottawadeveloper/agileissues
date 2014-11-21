abf1bc2d5f4e3681e71dd562a0314631cbe1751a
Robert Turnbull <rturnbul@uottawa.ca>
2014-11-21 13:56:34 -0500

    fixed the rendering

--- templates/agileissues-note-list.tpl.php
+++ templates/agileissues-note-list.tpl.php
@@ -1,12 +1,14 @@
 <?php 
 
-echo '<div id="agile-note-list">';
+echo '<div class="agile-note-list">';
 if (empty($list['#ids'])) {
   echo '<p>There are no notes to be shown.</p>';
 }
-foreach ($list['#ids'] as $id) {
-  $entity = entity_load('agile_note', array($id));
-  $view = entity_view('agile_note', $entity, 'full');
-  echo render($view);
+else {
+  echo '<ul>';
+  foreach ($list['#children'] as $child) {
+    echo render($child);
+  }
+  echo '</ul>';
 }
 echo '</div>';
