17387ea005647ba477f0bdc052c72eee1554eaed
Robert Turnbull <rturnbul@uottawa.ca>
2014-11-27 14:08:38 -0500

    updated search templates and beginning of privacy

--- templates/agileissues-search-results.tpl.php
+++ templates/agileissues-search-results.tpl.php
@@ -2,8 +2,10 @@
 
   $result_objects = entity_load($results['#result']['entity_type'], array_keys($results['#result']['ids']));
   
+  $mode = agileissues_frontend_access() ? 'teaser' : 'teaser-public';
+  
   echo '<div class="agile-search-results">';
-  $view = entity_view($results['#result']['entity_type'], $result_objects, 'teaser');
+  $view = entity_view($results['#result']['entity_type'], $result_objects, $mode);
   foreach ($view as $thing) {
     echo render($thing);
   }
