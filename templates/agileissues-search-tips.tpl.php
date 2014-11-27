874ac407cd830c05ee73451f32bc1c14a7c237e4
Robert Turnbull <rturnbul@uottawa.ca>
2014-11-27 11:52:53 -0500

    default template

--- templates/agileissues-search-tips.tpl.php
+++ templates/agileissues-search-tips.tpl.php
@@ -1,8 +1,17 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<h1>Search Tips</h1>
+<h2>Stories</h2>
+<dl>
+  <?php 
+    foreach ($tips['#stories'] as $tip => $def) {
+      echo '<dt>' . $tip . '</dt><dd>' . $def . '</dd>';
+    }
+  ?>
+</dl>
+<h2>Tasks</h2>
+<dl>
+  <?php 
+    foreach ($tips['#tasks'] as $tip => $def) {
+      echo '<dt>' . $tip . '</dt><dd>' . $def . '</dd>';
+    }
+  ?>
+</dl>
