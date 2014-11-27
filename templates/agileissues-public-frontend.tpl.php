857da8aaf56c13d11c5048f2bbdde87413733eee
Robert Turnbull <rturnbul@uottawa.ca>
2014-11-27 13:14:49 -0500

    tweaked view mode name

--- templates/agileissues-public-frontend.tpl.php
+++ templates/agileissues-public-frontend.tpl.php
@@ -1,1 +1,10 @@
-<?php
+<?php echo theme('agileissues_bar', array('mode' => 'public')); ?>
+<ul class="agile-frontend-project-list agile-frontend-project-list--public">
+  <?php
+  foreach ($frontend['#projects'] as $project) {
+    echo '<li>';
+    echo render($project);
+    echo '</li>';
+  }
+  ?>
+</ul>
